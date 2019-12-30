c169a70fbff86e0490d450ec3a32d7c6df918505
avinash sahu <avi.sahu2009@gmail.com>
2019-12-30 10:57:41 +0530

    hos service

--- src/test/java/com/hos/hosuserservice/HosUserServiceApplicationTests.java
+++ src/test/java/com/hos/hosuserservice/HosUserServiceApplicationTests.java
@@ -1,38 +1,28 @@
 package com.hos.hosuserservice;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.mockito.Mockito.*;
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import java.util.Arrays;
-import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import org.springframework.http.*;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -55,5 +45,17 @@ public class HosUserServiceApplicationTests {
 	        .andExpect(jsonPath("$[0].lastName", is("Suman")))
 	        .andExpect(jsonPath("$[0].firstName", is("Choudhury")))
 	        .andExpect(jsonPath("$[0].email", is("kjkj@gmail.com")));
+		 verify(userService,times(1)).getAllUsers();
+	}
+	
+	@Test
+	public void testSaveUser() throws Exception {
+		when(userService.saveUser(any(User.class))).thenReturn("Success");
+		this.mockMvc.perform(post("/user")
+				.content("{\"id\": 1,\"email\": \"kjkj@gmail.com\",\"firstName\": \"Suman\",\"lastName\": \"Choudhury\"}").header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andReturn().getResponse().getContentAsString().contentEquals("Success");
+		verify(userService,times(1)).saveUser(any(User.class));
 	}
 }
